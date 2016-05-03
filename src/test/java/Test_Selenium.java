e325f8ec302a79e9b076c5101f82b825b86a14fc
Silas Cagatay <silas_cagatay@hotmail.com>
2016-05-03 14:50:09 +0200

    First commit Playground

---
+++ src/test/java/Test_Selenium.java
@@ -0,0 +1,111 @@
+import static org.junit.Assert.*;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import Uppgift.Selenium;
+
+public class Test_Selenium {
+
+	static Selenium start;
+
+
+	@BeforeClass
+	public static void BeforeClass(){
+		start = new Selenium();
+	}
+
+	@Test
+	public void task1() {
+		start.task1();
+
+	}
+
+	@Test
+	public void task2() {
+		start.task2();
+
+	}
+
+	@Test
+	public void task3() {
+		start.task3();
+
+	}
+
+	@Test
+	public void task4() {
+		start.task4();
+
+	}
+
+	@Test
+	public void task5() {
+		start.task5();
+
+	}
+
+	@Test
+	public void task6() {
+		start.task6();
+
+	}
+
+	@Test
+	public void task7() {
+		start.task7();
+
+	}
+
+	@Test
+	public void task8() {
+		start.task8();
+
+	}
+
+	@Test
+	public void task9() {
+		start.task9();
+
+	}
+
+	@Test
+	public void task10() {
+		start.task10();
+
+	}
+
+	@Test
+	public void task11() {
+		start.task11();
+
+	}
+	@Test
+	public void task12() {
+		start.task12();
+
+	}
+	@Test
+	public void task13() {
+		start.task13();
+
+	}
+	@Test
+	public void task14() {
+		start.task14();
+
+	}
+	@Test
+	public void task1516() {
+		start.task1516();
+
+	}
+	
+	@Test
+	public void task17() {
+		start.task17();
+
+	}
+
+
+}
